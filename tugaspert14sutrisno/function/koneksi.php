90180b78c94446489f1d98249e8092c72a82657f
SutrisnoPanuluh <115914654+SutrisnoPanuluh@users.noreply.github.com>
2023-01-10 20:27:32 +0700

    Add files via upload

---
+++ tugaspert14sutrisno/function/koneksi.php
@@ -0,0 +1,10 @@
+
+
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tugaspert14sutrisno";
+
+$koneksi = mysqli_connect($server, $username, $password, $dbname) or die("Koneksi ke database gagal");
